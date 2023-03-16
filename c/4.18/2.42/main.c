999e4eb2e5c98b401f247b9001a3b65738dd8195
slavamuravey <slavamuravey@mail.ru>
2023-03-16 19:42:54 +0700

    Finish 4.18/2.42 task

---
+++ c/4.18/2.42/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t my_strlen(const char *s)
+{
+    const char *p;
+    for (p = s; *p; p++) {}
+
+    return p - s;
+}
+
+void append_char(char *dest, char ch)
+{
+    size_t len = my_strlen(dest);
+    dest[len] = ch;
+    dest[len + 1] = '\0';
+}
+
+struct word {
+    char *str;
+    struct word *next;
+};
+
+int main(int argc, char **argv)
+{
+    int c;
+    int is_word = 0;
+    char *word;
+    struct word *first = NULL, *tmp;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n') {
+            if (is_word == 1) {
+                tmp = malloc(sizeof(struct word));
+                tmp->str = word;
+                tmp->next = first;
+                first = tmp;
+            }
+
+            is_word = 0;
+        } else {
+            if (is_word == 0) {
+                word = malloc(sizeof(char) + 1);
+                *word = '\0';
+            } else {
+                word = realloc(word, my_strlen(word) + 1);
+            }
+
+            append_char(word, c);
+
+            is_word = 1;
+        }
+
+
+        if (c == '\n') {
+            while (first) {
+                printf("%s ", first->str);
+                tmp = first;
+                first = first->next;
+                free(tmp->str);
+                free(tmp);
+            }
+            putchar('\n');
+        }
+    }
+
+    return 0;
+}
