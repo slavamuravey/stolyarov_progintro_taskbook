1f10da8ccc3af466547edc2d63337dcc49f4c126
slavamuravey <slavamuravey@mail.ru>
2023-03-18 14:25:19 +0700

    Fix 4.18/2.42: change size to allocate new string considering trailing zero char

--- c/4.18/2.42/main.c
+++ c/4.18/2.42/main.c
@@ -9,6 +9,26 @@ size_t my_strlen(const char *s)
     return p - s;
 }
 
+char *my_strcpy(char *dest, const char *src)
+{
+    char *p = dest;
+    for (; *src; src++, p++) {
+        *p = *src;
+    }
+
+    *p = '\0';
+
+    return dest;
+}
+
+char *my_stralloc(const char *src, size_t size)
+{
+    char *dest = malloc(size);
+    my_strcpy(dest, src);
+
+    return dest;
+}
+
 void append_char(char *dest, char ch)
 {
     size_t len = my_strlen(dest);
@@ -40,12 +60,14 @@ int main(int argc, char **argv)
             is_word = 0;
         } else {
             if (is_word == 0) {
-                word = malloc(sizeof(char) + 1);
-                *word = '\0';
+                word = my_stralloc("", 1 + 1);
             } else {
-                word = realloc(word, my_strlen(word) + 1);
+                char *new_word = my_stralloc(word, my_strlen(word) + 1 + 1);
+                free(word);
+                word = new_word;
+                // word = realloc(word, my_strlen(word) + 1 + 1);
             }
-
+            
             append_char(word, c);
 
             is_word = 1;
