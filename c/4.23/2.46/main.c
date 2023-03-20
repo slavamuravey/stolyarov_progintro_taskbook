1f09e99e50bb1fcd1bf0a66735dafab0c31da937
slavamuravey <slavamuravey@mail.ru>
2023-03-20 13:11:51 +0700

    Finish 4.23/2.46 task

---
+++ c/4.23/2.46/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+    char *text = 
+        "Humpty Dumpty sat on a wall,\n"
+        "Humpty Dumpty had a great fall.\n"
+        "Four-score Men and Four-score more,\n"
+        "Could not make Humpty Dumpty where he was before.\n"
+    ;
+
+    if (argc != 2) {
+        fprintf(stderr, "Too few arguments.\n");
+        exit(1);
+    }
+
+    char *filename = argv[1];
+    FILE *f = fopen(filename, "a");
+    if (!f) {
+        perror(filename);
+        exit(1);
+    }
+
+    for (; *text; text++) {
+        fputc(*text, f);
+    }
+
+    fclose(f);
+
+    return 0;
+}
