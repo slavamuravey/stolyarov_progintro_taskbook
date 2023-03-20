56b734107c9e9df1afbbe0013234e8622b164a4e
slavamuravey <slavamuravey@mail.ru>
2023-03-20 14:32:06 +0700

    Finish 4.23/2.49 task

---
+++ c/4.23/2.49/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        fprintf(stderr, "Too few arguments.\n");
+        exit(1);
+    }
+
+    char *filename = argv[1];
+    FILE *f = fopen(filename, "w");
+    if (!f) {
+        perror(filename);
+        exit(1);
+    }
+
+    int c;
+    while ((c = getchar()) != EOF) {
+        fputc(c, f);
+    }
+
+    fclose(f);
+
+    return 0;
+}
