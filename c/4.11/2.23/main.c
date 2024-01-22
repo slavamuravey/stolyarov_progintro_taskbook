b07257f0e1b8f089c4ab00047d4d37235da03be1
slavamuravey <slavamuravey@mail.ru>
2024-01-22 17:41:41 +0700

    Finish 4.11/2.23 task

---
+++ c/4.11/2.23/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    char *str, *p;
+    size_t wc;
+    int is_word;
+
+    if (argc < 2) {
+        fprintf(stderr, "Too few arguments.\n");
+        exit(1);
+    }
+
+    wc = 0;
+    str = argv[1];
+    is_word = 0;
+
+    for (p = str; *p; p++) {
+        if (isspace(*p) || *p == '\n') {
+            if (is_word) {
+                is_word = 0;
+                wc++;
+            }
+        } else {
+            is_word = 1;
+        }
+    }
+
+    if (is_word) {
+        wc++;
+    }
+
+    printf("%ld\n", wc);
+
+    return 0;
+}
